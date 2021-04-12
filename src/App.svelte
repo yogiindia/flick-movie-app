50265c6b2f32a5a3454f6bf409f4630806887a5f
yogiindia <yogeshkumar30987@gmail.com>
2021-04-12 17:39:20 +0530

    project init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from './components/header.svelte'
+	export let name;
+</script>
+
+<main>
+	<Header {name} />
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+
+<style global type="text/postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
+
+
